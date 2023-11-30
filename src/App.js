069c2f0ff06b1f69ac08ba0a4df09d74397d89c8
Shangjin Tang <shangjin.tang@gmail.com>
2023-11-30 19:04:50 +0800

    Use leaflet to add a layer from geoserver with OpenStreetMap coastlines

--- src/App.js
+++ src/App.js
@@ -1,25 +1,81 @@
-import logo from './logo.svg';
-import './App.css';
+import React, {useEffect, useRef, useState} from 'react';
+import {MapContainer, TileLayer, Polygon} from 'react-leaflet';
+import 'leaflet/dist/leaflet.css';
+import L from 'leaflet';
+import 'leaflet-geoserver-request/src/L.Geoserver';
+
+function Map() {
+    const mapRef = useRef(null);
+    const [showDefaultLayer, setShowDefaultLayer] = useState(true);
+    const [showCoastlineLayer, setShowCoastlineLayer] = useState(false);
+
+    useEffect(() => {
+        let map = null;
+        map = mapRef.current;
+        if (!map) {
+            return;
+        }
+
+        const coastlineLayer = L.Geoserver.wms('http://localhost:8080/geoserver/wms', {
+            layers: 'osm:osm_coastlines',
+        });
+
+        if (showCoastlineLayer) {
+            console.log("add coastline layer to map")
+            coastlineLayer.addTo(map);
+        } else {
+            console.log("remove coastline layer from map")
+            coastlineLayer.removeFrom(map);
+        }
+
+        if (showCoastlineLayer) {
+            console.log("add coastline layer to map")
+            coastlineLayer.addTo(map);
+        } else {
+            console.log("remove coastline layer from map")
+            coastlineLayer.removeFrom(map);
+        }
+
+
+        return () => {
+            coastlineLayer.removeFrom(map); // 清除 CoastlineLayer
+        };
+    }, [showCoastlineLayer]);
+
+    const toggleDefaultLayer = () => {
+        setShowDefaultLayer((prevShowDefaultLayer) => !prevShowDefaultLayer);
+    };
+
+    const toggleCoastlineLayer = () => {
+        setShowCoastlineLayer((prevShowCoastlineLayer) => !prevShowCoastlineLayer);
+    };
+
+    return (
+        <div>
+            <label>
+                <input type="checkbox" checked={showDefaultLayer} onChange={toggleDefaultLayer}/>
+                Default Layer
+            </label>
+            <br/>
+            <label>
+                <input type="checkbox" checked={showCoastlineLayer} onChange={toggleCoastlineLayer}/>
+                Coastline Layer
+            </label>
+            <MapContainer id="map" className="map-container" ref={mapRef}
+                          center={[30.5047, 122.2981]} zoom={11}>
+                {showDefaultLayer && <TileLayer url="https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png"/>}
+            </MapContainer>
+        </div>
+    );
+}
 
 function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+    return (
+        <div className="App">
+            <h1>Map with Leaflet</h1>
+            <Map/>
+        </div>
+    );
 }
 
 export default App;
